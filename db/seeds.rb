6af73cf8f4c804182dad0a358246aa4528b5e96a
LynnAmsbury <Lynn8542@gmail.com>
2020-07-10 10:53:15 -0600

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
+Wine.destroy_all
+
+Wine.create({
+    varietal: "Cabernet Sauvignon",
+    producer: "Joseph Phelps Vineyards",
+    region: "Backus Vineyard, Napa Valley",
+    vintage: 2016,
+    number_in_collection: 6
+})
+
 Wine.create({
     varietal: "Cabernet Sauvignon",
     producer: "Outpost",
     region: "Howell Mountain, Napa Valley",
     vintage: 2014,
     number_in_collection: 5
+})
+
+Wine.create({
+    varietal: "Pinot Noir",
+    producer: "Emeritus",
+    region: "Hallberg Ranch, Russian River Valley",
+    vintage: 2017,
+    number_in_collection: 1
 })
 
 Wine.create({
